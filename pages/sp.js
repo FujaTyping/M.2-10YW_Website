269315afcfc6b9723657147f6e9bcc84852ea082
Siraphop Sukchu <86290693+FujaTyping@users.noreply.github.com>
2022-07-15 14:38:47 +0700

    Delete sp.js

--- pages/sp.js
+++
@@ -1,9 +0,0 @@
-const Scsong = new Audio() ;
-Scsong.src = "../Img/HYWsong.mp3" ;
-
-function playsong() {
-    console.log("Songplay!");
-    Scsong.play();
-    console.log("Loop /");
-    Scsong.loop = true
-}  
